31e37930929feba8dbc729e5a4cf22eb2d46cd31
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-06-23 20:35:52 -0300

    Update Veterinario.java

--- src/main/java/org/uepb/entity/Veterinario.java
+++ src/main/java/org/uepb/entity/Veterinario.java
@@ -3,5 +3,14 @@ package org.uepb.entity;
 public class Veterinario extends Funcionario {
     public Veterinario(String nome, String especialidade, String cpf) {
         super(nome, especialidade, cpf);
+        
     }
+    
+    @Override
+	public String toString() {
+		return "Veterinário \nNome: " + getNome() + 
+				"\nEspecialidade: " + getEspecialidade() + 
+				"\nCPF: " + getCpf();
+	}
+    
 }
