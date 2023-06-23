f65130e9a21b884f1b028bb95214ab3596c8e2ac
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-06-23 20:36:12 -0300

    Update Groomer.java

--- src/main/java/org/uepb/entity/Groomer.java
+++ src/main/java/org/uepb/entity/Groomer.java
@@ -4,4 +4,13 @@ public class Groomer extends Funcionario {
     public Groomer(String nome, String especialidade, String cpf) {
         super(nome, especialidade, cpf);
     }
+
+	@Override
+	public String toString() {
+		return "Groomer \nNome: " + getNome() + 
+				"\nEspecialidade: " + getEspecialidade() + 
+				"\nCPF: " + getCpf();
+	}
+    
+    
 }
