28d38f767b28627b200849560132db93da88ed74
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-06-24 18:50:43 -0300

    Update Sistema.java

--- src/main/java/org/uepb/controller/Sistema.java
+++ src/main/java/org/uepb/controller/Sistema.java
@@ -1,77 +1,122 @@
 package org.uepb.controller;
 
-import org.uepb.entity.Atendimento;
-import org.uepb.entity.Funcionario;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sistema {
-	private ArrayList<Funcionario> funcionarios = new ArrayList<>();
+	private ArrayList<Groomer> funcionarios = new ArrayList<>();
+	private Veterinario veterinario;
 
 	public Sistema() {
 		super();
-		this.funcionarios = new ArrayList<Funcionario>();
+		this.funcionarios = new ArrayList<Groomer>();
 	}
 
-	public ArrayList<Funcionario> getFuncionarios() {
+	public Veterinario getVeterinario() {
+		return veterinario;
+	}
+	
+	public void setVeterinario(Veterinario veterinario) {
+		this.veterinario = veterinario;
+	}
+	
+	public ArrayList<Groomer> getFuncionarios() {
 		return funcionarios;
 	}
 
-	public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
+	public void setFuncionarios(ArrayList<Groomer> funcionarios) {
 		this.funcionarios = funcionarios;
 	}
 	
 	public void adicionarAtendimento(Atendimento atendimento) {
 		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> ID = new ArrayList<>();
 		int choice = 0;
-		if (!funcionarios.isEmpty()) {
-			for (Funcionario funcionario : funcionarios) {
-				if (funcionario.getEspecialidade() == atendimento.getAnimalPaciente().getEspecie()) {
-					System.out.println("ID: 00" + (funcionarios.indexOf(funcionario)+1) + "\nNome: " + funcionario.getNome() + "\nCPF: " + funcionario.getCpf());
+		while (choice < 1 || choice > 2 ) {
+			System.out.println("\nO atendimento será com um groomer ou veterinário?\n[1] Groomer | [2] Veterinário: ");
+			choice = sc.nextInt();
+		}
+		if (choice == 1) {
+			choice = 0;
+			if (!funcionarios.isEmpty()) {
+				for (Groomer groomer : funcionarios) {
+					if (groomer.getEspecialidade() == atendimento.getAnimalPaciente().getEspecie()) {
+						System.out.println("ID: 00" + (funcionarios.indexOf(groomer)+1) + "\nNome: " + groomer.getNome() + "\nCPF: " + groomer.getCpf());
+						ID.add(funcionarios.indexOf(groomer)+1);
+					}
 				}
-			}
-			while (true) {
-				System.out.println("Digite o ID do funcionário escolhido: ");
-				choice = sc.nextInt();
-				if (choice >= 1 && choice <= funcionarios.size()){
-					System.out.println("Saiu");
-					break;
+				while (true) {
+					System.out.println("Digite o ID do funcionário escolhido: ");
+					choice = sc.nextInt();
+					if (choice >= 1 && choice <= funcionarios.size() && ID.contains(choice)){
+						break;
+					}
+					
 				}
-				
+				funcionarios.get(choice-1).getAgenda().getAtendimentos().add(atendimento);
+				System.out.println("O atendimento com o Groomer foi marcado com sucesso!\n");
 			}
-			funcionarios.get(choice-1).getAgenda().getAtendimentos().add(atendimento);
-			System.out.println("Atendimento marcado com sucesso!\n");
+		}
+		else {
+			choice = 0;
+			veterinario.getAgenda().getAtendimentos().add(atendimento);
+			System.out.println("O atendimento com o Veterinário foi marcado com sucesso!\n");
 		}
 	}
 	
 	public void removerAtendimento(String nomeAnimal, String nomeDono) {
 		Scanner sc = new Scanner(System.in);
 		int choice = 0;
-		if (!funcionarios.isEmpty()) {
-			for (Funcionario funcionario : funcionarios) {
-				System.out.println((funcionarios.indexOf(funcionario)+1) + "º \nNome: " + funcionario.getNome() + "\nCPF: " + funcionario.getCpf());
-			}
-			while (true) {
-				System.out.println("Digite o número do funcionário: ");
-				choice = sc.nextInt();
-				if (choice >= 1 && choice <= funcionarios.size());{
-					break;
+		while (choice < 1 || choice > 2 ) {
+			System.out.println("\nO atendimento será com um groomer ou veterinário?\n[1] Groomer | [2] Veterinário: ");
+			choice = sc.nextInt();
+		}
+		if (choice == 1) {
+			if (!funcionarios.isEmpty()) {
+				for (Funcionario funcionario : funcionarios) {
+					System.out.println((funcionarios.indexOf(funcionario)+1) + "º \nNome: " + funcionario.getNome() + "\nCPF: " + funcionario.getCpf());
+				}
+				while (true) {
+					System.out.println("Digite o número do funcionário: ");
+					choice = sc.nextInt();
+					if (choice >= 1 && choice <= funcionarios.size());{
+						break;
+					}
+				}
+				for (Atendimento atendimento : funcionarios.get(choice-1).getAgenda().getAtendimentos()) {
+					if (atendimento.getAnimalPaciente().getNome() == nomeAnimal && atendimento.getAnimalPaciente().getDono().getNome() == nomeDono) {
+						funcionarios.get(choice-1).getAgenda().getAtendimentos().remove(atendimento);
+						break;
+					}
 				}
 			}
-			for (Atendimento atendimento : funcionarios.get(choice-1).getAgenda().getAtendimentos()) {
+		}
+		else {
+			for (Atendimento atendimento : veterinario.getAgenda().getAtendimentos()) {
 				if (atendimento.getAnimalPaciente().getNome() == nomeAnimal && atendimento.getAnimalPaciente().getDono().getNome() == nomeDono) {
-					funcionarios.get(choice-1).getAgenda().getAtendimentos().remove(atendimento);
+					veterinario.getAgenda().getAtendimentos().remove(atendimento);
 					break;
 				}
 			}
 		}
 	}
 	public void realizarAtendimento(Funcionario funcionario, int index) {
-		funcionario.getAgenda().getAtendimentos().remove(index-1);
+		if (!funcionario.getAgenda().getAtendimentos().isEmpty()) {
+			System.out.println("\nA consulta com o pet cujo dono se chama: " + funcionario.getAgenda().getAtendimentos().get(index-1).getAnimalPaciente().getDono().getNome() + " e com o pet de nome: " + funcionario.getAgenda().getAtendimentos().get(index-1).getAnimalPaciente().getNome() + " foi realizada com sucesso!");
+			funcionario.getAgenda().getAtendimentos().remove(index-1);
+		}
+		else {
+			System.out.println("A agenta está vazia!");
+		}
 	}
 	
-	public void realizarAssistênciaVeterianaria() {
-		
+	public void realizarAssistênciaVeterianaria(int index) {
+		if (!veterinario.getAgenda().getAtendimentos().isEmpty()) {
+			System.out.println("\nA consulta com o pet cujo dono se chama: " + veterinario.getAgenda().getAtendimentos().get(index-1).getAnimalPaciente().getDono().getNome() + " e com o pet de nome: " + veterinario.getAgenda().getAtendimentos().get(index-1).getAnimalPaciente().getNome() + " foi realizada com sucesso!");
+			veterinario.getAgenda().getAtendimentos().remove(index-1);
+		}
+		else {
+			System.out.println("A agenta está vazia!");
+		}
 	}
 }
