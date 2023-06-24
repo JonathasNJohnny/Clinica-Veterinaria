9c0063bfeb364a8636323e42343ce2710db2dfcd
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-06-24 17:04:11 -0300

    Atualizada a classe Veterinario.java

--- src/main/java/org/uepb/entity/Veterinario.java
+++ src/main/java/org/uepb/entity/Veterinario.java
@@ -1,15 +1,24 @@
 package org.uepb.entity;
 
 public class Veterinario extends Funcionario {
-    public Veterinario(String nome, String cpf) {
-        super(nome, cpf);
-        
+    public Veterinario(VeterinarioBuilder builder) {
+        super(builder);
     }
-    
+
     @Override
-	public String toString() {
-		return "Veterinário \nNome: " + getNome() + 
-				"\nCPF: " + getCpf();
-	}
-    
+    public String toString() {
+        return "Veterinário\nNome: " + getNome() + "\nCPF: " + getCpf();
+    }
+
+    public static class VeterinarioBuilder extends FuncionarioBuilder<Veterinario, VeterinarioBuilder> {
+        @Override
+        protected VeterinarioBuilder self() {
+            return this;
+        }
+
+        @Override
+        public Veterinario build() {
+            return new Veterinario(this);
+        }
+    }
 }
