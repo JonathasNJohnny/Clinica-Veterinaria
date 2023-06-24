adf9dafc438f1db40827f30672635e95734fcde7
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-06-24 19:19:09 -0300

    Atualizada a classe Funcionario.java e Groomer.java

--- src/main/java/org/uepb/entity/Funcionario.java
+++ src/main/java/org/uepb/entity/Funcionario.java
@@ -2,16 +2,15 @@ package org.uepb.entity;
 
 public abstract class Funcionario {
     private String nome;
-    private Agenda agenda = new Agenda();
+    private Agenda agenda;
     private String cpf;
 
-    public Funcionario(String nome,  String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.agenda = new Agenda();
-        this.agenda.setNomeDono(nome);
+    public Funcionario(FuncionarioBuilder<?, ?> builder) {
+        this.nome = builder.nome;
+        this.agenda = builder.agenda;
+        this.cpf = builder.cpf;
     }
-    
+
     public String getNome() {
         return nome;
     }
@@ -34,5 +33,30 @@ public abstract class Funcionario {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public static abstract class FuncionarioBuilder<T extends Funcionario, B extends FuncionarioBuilder<T, B>> {
+        private String nome;
+        private Agenda agenda = new Agenda();
+        private String cpf;
+
+        protected abstract B self();
+
+        public B nome(String nome) {
+            this.nome = nome;
+            return self();
+        }
+
+        public B agenda(Agenda agenda) {
+            this.agenda = agenda;
+            return self();
+        }
+
+        public B cpf(String cpf) {
+            this.cpf = cpf;
+            return self();
+        }
+
+        public abstract T build();
     }
 }
