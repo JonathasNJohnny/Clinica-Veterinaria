25a5078bd93d00f45ef2a96d2fb9ac5faf43c131
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-06-24 18:52:09 -0300

    Update Agenda.java

--- src/main/java/org/uepb/entity/Agenda.java
+++ src/main/java/org/uepb/entity/Agenda.java
@@ -4,24 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Agenda {
-	private List<Atendimento> atendimentos;
-	private String nomeDono;
+    private ArrayList<Atendimento> atendimentos = new ArrayList<>();
+    private String nomeDono;
 
-	private Agenda(Builder builder) {
-		this.atendimentos = builder.atendimentos;
-		this.nomeDono = builder.nomeDono;
+	public Agenda() {
+		super();
+		this.atendimentos = new ArrayList<Atendimento>();
 	}
 
-	public Agenda() {}
-
-	public List<Atendimento> getAtendimentos() {
+	public ArrayList<Atendimento> getAtendimentos() {
 		return atendimentos;
 	}
 
-	public void setAtendimentos(List<Atendimento> atendimentos) {
+	public void setAtendimentos(ArrayList<Atendimento> atendimentos) {
 		this.atendimentos = atendimentos;
 	}
-
+	
 	public String getNomeDono() {
 		return nomeDono;
 	}
@@ -32,36 +30,13 @@ public class Agenda {
 
 	public void imprimeAtendimentos(){
 		if (!atendimentos.isEmpty()) {
-			System.out.println(nomeDono);
+			System.out.println("Agenda: " + nomeDono);
 			for (Atendimento atendimento : atendimentos) {
 				System.out.println((atendimentos.indexOf(atendimento)+1) + "º " + atendimento);
 			}
 		}
 		else {
 			System.out.println("A agenda está vazia!\n");
-		}
-	}
-
-	public static class Builder {
-		private List<Atendimento> atendimentos;
-		private String nomeDono;
-
-		public Builder() {
-			this.atendimentos = new ArrayList<>();
-		}
-
-		public Builder atendimentos(List<Atendimento> atendimentos) {
-			this.atendimentos = atendimentos;
-			return this;
-		}
-
-		public Builder nomeDono(String nomeDono) {
-			this.nomeDono = nomeDono;
-			return this;
-		}
-
-		public Agenda build() {
-			return new Agenda(this);
 		}
 	}
 }
