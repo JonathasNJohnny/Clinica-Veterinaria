c1b316df29d57d93b8734466176c7f3bb0caa12e
Jonathas Nóbrega <57995489+JonathasNJohnny@users.noreply.github.com>
2023-06-24 16:48:43 -0300

    Update Funcionario.java

--- src/main/java/org/uepb/entity/Funcionario.java
+++ src/main/java/org/uepb/entity/Funcionario.java
@@ -1,17 +1,13 @@
 package org.uepb.entity;
 
-import org.uepb.entity.Agenda.Builder;
-
 public abstract class Funcionario {
     private String nome;
-    private String especialidade;
     private Agenda agenda = new Agenda();
     private String cpf;
 
-    protected Funcionario(Builder builder) {
-        this.nome = builder.nome;
-        this.especialidade = builder.especialidade;
-        this.cpf = builder.cpf;
+    public Funcionario(String nome,  String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
         this.agenda = new Agenda();
         this.agenda.setNomeDono(nome);
     }
@@ -22,14 +18,6 @@ public abstract class Funcionario {
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public String getEspecialidade() {
-        return especialidade;
-    }
-
-    public void setEspecialidade(String especialidade) {
-        this.especialidade = especialidade;
     }
 
     public Agenda getAgenda() {
@@ -46,30 +34,5 @@ public abstract class Funcionario {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-
-    public static abstract class Builder<T extends Funcionario, B extends Builder<T, B>> {
-        private String nome;
-        private String especialidade;
-        private String cpf;
-
-        protected abstract B self();
-
-        public B nome(String nome) {
-            this.nome = nome;
-            return self();
-        }
-
-        public B especialidade(String especialidade) {
-            this.especialidade = especialidade;
-            return self();
-        }
-
-        public B cpf(String cpf) {
-            this.cpf = cpf;
-            return self();
-        }
-
-        public abstract T build();
     }
 }
