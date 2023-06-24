d35dcd9ccd61c3c7b75fc8ce2679b92ff270fd9f
fn1033 <Fn1033@protonmail.com>
2023-06-24 18:36:37 -0300

    Adicionado SistemaTest.

---
+++ src/test/java/SistemaTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.uepb.controller.Sistema;
+import org.uepb.entity.*;
+
+import java.util.ArrayList;
+
+class SistemaTest {
+    Sistema sis;
+
+    @BeforeEach
+    void setUp(){
+        ArrayList<Atendimento> atendimentos = new ArrayList<Atendimento>();
+
+        Agenda.Builder builderAgenda = new Agenda.Builder();
+        Agenda agenda = builderAgenda.nomeDono("Sansão Carrasco").atendimentos(atendimentos).build();
+
+        Groomer.GroomerBuilder builder = new Groomer.GroomerBuilder();
+        Groomer groomerTeste = builder.nome("Sansão Carrasco").cpf("123.456.789-12").especialidade("Cachorros").agenda(agenda).build();
+
+        ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+        funcionarios.add(groomerTeste);
+
+        sis = new Sistema();
+        sis.setFuncionarios(funcionarios);
+
+
+    }
+}
